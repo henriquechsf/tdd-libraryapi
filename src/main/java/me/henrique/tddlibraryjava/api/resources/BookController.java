2ca8fde8e5776eed34883661c2cfb5fd50527504
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-01-14 00:27:20 -0300

    add Model Mapper

--- src/main/java/me/henrique/tddlibraryjava/api/resources/BookController.java
+++ src/main/java/me/henrique/tddlibraryjava/api/resources/BookController.java
@@ -3,6 +3,7 @@ package me.henrique.tddlibraryjava.api.resources;
 import me.henrique.tddlibraryjava.api.dto.BookDTO;
 import me.henrique.tddlibraryjava.model.entity.Book;
 import me.henrique.tddlibraryjava.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,27 +13,19 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service) {
+    public BookController(BookService service, ModelMapper mapper) {
         this.service = service;
+        this.modelMapper = mapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto) {
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();
-
+        Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
 
-        return BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();
+        return modelMapper.map(entity, BookDTO.class);
     }
 }
