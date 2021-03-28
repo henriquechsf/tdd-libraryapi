799c840381c509cb6ff512094bc4afe16047432b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-03-28 18:08:10 -0300

    refact: alterações para receber o e-mail do customer na request

--- src/main/java/me/henrique/tddlibraryjava/TddLibraryJavaApplication.java
+++ src/main/java/me/henrique/tddlibraryjava/TddLibraryJavaApplication.java
@@ -4,13 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class TddLibraryJavaApplication {
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Scheduled(cron = "0 0 18 1/1 * ?")
+	public void testeAgendamento() {
+		System.out.println("Teste agendadmento de tarefas");
 	}
 
 	public static void main(String[] args) {
--- src/test/java/me/henrique/tddlibraryjava/api/resource/LoanControllerTest.java
+++ src/test/java/me/henrique/tddlibraryjava/api/resource/LoanControllerTest.java
@@ -61,7 +61,7 @@ public class LoanControllerTest {
     @DisplayName("Deve realizar um empréstimo")
     public void createLoanTest() throws Exception {
         // json
-        LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
+        LoanDTO dto = LoanDTO.builder().isbn("123").email("customer@email.com").customer("Fulano").build();
         String json = new ObjectMapper().writeValueAsString(dto);
 
         Book book = Book.builder().id(1L).isbn("123").build();
